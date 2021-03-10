35c2c54e8d2624862a10726f2d487ab726492aaa
Brian Sy <brianpesy@gmail.com>
2021-03-10 14:13:08 +0800

    Initial commit

---
+++ beautymnl-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  beautymnl-test
+//
+//  Created by Brian Nicholas Sy on 3/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // appear
+    }
+
+
+}
+
